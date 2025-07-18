36a5261638bd3fed89d672cfefe86f1faf80e0ab
simondlevy <simon.d.levy@gmail.com>
2025-07-17 21:46:04 -0400

    =More risp.cpp

--- include/risp.hpp
+++ include/risp.hpp
@@ -621,66 +621,181 @@ namespace risp
 
             ~Processor() { }
 
-            bool load_network(neuro::Network* n, int network_id = 0);
+            bool load_network(neuro::Network* net, int network_id) {
 
-            bool load_networks(std::vector<neuro::Network*> &n);
+                if (networks.find(network_id) != networks.end()) delete networks[network_id];
 
-            void clear(int network_id = 0);
+                risp::Network * risp_net = new risp::Network(net, 
+                        spike_value_factor,
+                        min_potential,
+                        leak_mode[0], 
+                        run_time_inclusive, 
+                        threshold_inclusive, 
+                        fire_like_ravens, 
+                        discrete, 
+                        inputs_from_weights,
+                        noisy_seed, 
+                        noisy_stddev,
+                        weights,
+                        stds);
+                networks[network_id] = risp_net;
 
-            void apply_spike(const Spike& s, bool normalized = true, int network_id = 0);
+                return true;
+            }
 
-            void apply_spike(const Spike& s, const vector<int>& network_ids, bool normalized = true);
+            bool load_networks(std::vector<neuro::Network*> &n) {
+                size_t i, j;
 
-            void apply_spikes(const vector<Spike>& s, bool normalized = true, int network_id = 0);
+                for (i = 0; i < n.size(); i++) {
+                    if(load_network(n[i], i) == false) {
+                        for (j = 0; j <= i; j++) {
+                            delete networks[j];
+                            networks.erase(j);
+                        }
+                        return false;
+                    }
+                }
+                return true;
+            }
 
-            void apply_spikes(const vector<Spike>& s, const vector<int>& network_ids, bool normalized = true);
+            void clear(int network_id) {
+                risp::Network *risp_net = get_risp_network(network_id);
+                networks.erase(network_id);
+                delete risp_net;
+            }
 
-            void run(double duration, int network_id = 0);
 
-            void run(double duration, const vector<int>& network_ids);
 
-            double get_time(int network_id = 0);
+            void apply_spike(const Spike& s, bool normalize, int network_id) {
+                get_risp_network(network_id)->apply_spike(s, normalize);
+            }
 
-            bool track_output_events(int output_id, bool track = true, int network_id = 0);
+            void apply_spike(const Spike& s, 
+                    const vector<int>& network_ids,
+                    bool normalize) {
+                size_t i;
 
-            bool track_neuron_events(uint32_t node_id, bool track = true, int network_id = 0);
+                for (i = 0; i < network_ids.size(); i++) {
+                    apply_spike(s, normalize, network_ids[i]);
+                }
+            }
 
-            double output_last_fire(int output_id, int network_id = 0);
+            void apply_spikes(const vector<Spike>& s, bool normalize, int network_id) {
+                size_t i;
+                risp::Network *risp_net = get_risp_network(network_id);
 
-            vector <double> output_last_fires(int network_id = 0);
+                for (i = 0; i < s.size(); i++) {
+                    risp_net->apply_spike(s[i], normalize);
+                }
+            }
 
-            int output_count(int output_id, int network_id = 0);
 
-            vector <int> output_counts(int network_id = 0);
+            void apply_spikes(const vector<Spike>& s, 
+                    const vector<int>& network_ids,
+                    bool normalize) {
+                size_t i;
+                for (i = 0; i < network_ids.size(); i++) {
+                    apply_spikes(s, normalize, network_ids[i]);
+                }
+            }
 
-            vector <double> output_vector(int output_id, int network_id = 0);
 
-            vector < vector <double> > output_vectors(int network_id = 0);
+            void run(double duration, int network_id) {
+                get_risp_network(network_id)->run(duration);
+            }
 
-            long long total_neuron_counts(int network_id = 0);
+            void run(double duration, const vector<int>& network_ids) {
+                size_t i;
+                for (i = 0; i < network_ids.size(); i++) {
+                    get_risp_network(network_ids[i])->run(duration);
+                }
+            }
 
-            long long total_neuron_accumulates(int network_id = 0);
 
-            vector <int> neuron_counts(int network_id = 0);
+            long long total_neuron_counts(int network_id) {
+                return get_risp_network(network_id)->total_neuron_counts();
+            }
 
-            vector <double> neuron_last_fires(int network_id = 0);
+            long long total_neuron_accumulates(int network_id) {
+                return get_risp_network(network_id)->total_neuron_accumulates();
+            }
 
-            vector < vector <double> > neuron_vectors(int network_id = 0);
+            double get_time(int network_id) {
+                return get_risp_network(network_id)->get_time();
+            }
 
-            vector < double > neuron_charges(int network_id = 0);
+            bool track_output_events(int output_id, bool track, int network_id) {
+                return get_risp_network(network_id)->track_output_events(output_id, track);
+            }
 
-            void synapse_weights(vector <uint32_t> &pres,
+            bool track_neuron_events(uint32_t node_id, bool track, int network_id) {
+                return get_risp_network(network_id)->track_neuron_events(node_id, track);
+            }
+
+            double output_last_fire(int output_id, int network_id) {
+                return get_risp_network(network_id)->output_last_fire(output_id);
+            }
+
+            vector <double> output_last_fires(int network_id) {
+                return get_risp_network(network_id)->output_last_fires();
+            }
+
+            int output_count(int output_id, int network_id) {
+                return get_risp_network(network_id)->output_count(output_id);
+            }
+
+            vector <int> output_counts(int network_id) {
+                return get_risp_network(network_id)->output_counts();
+            }
+
+            vector <double> output_vector(int output_id, int network_id) {
+                return get_risp_network(network_id)->output_vector(output_id);
+            }
+
+            vector < vector <double> > output_vectors(int network_id) {
+                return get_risp_network(network_id)->output_vectors();
+            }
+
+
+            vector <int> neuron_counts(int network_id) {
+                return get_risp_network(network_id)->neuron_counts();
+            }
+
+            vector < vector <double> > neuron_vectors(int network_id) {
+                return get_risp_network(network_id)->neuron_vectors();
+            }
+            vector < double > neuron_charges(int network_id) {
+                return get_risp_network(network_id)->neuron_charges();
+            }
+
+            vector <double> neuron_last_fires(int network_id) {
+                return get_risp_network(network_id)->neuron_last_fires();
+            }
+
+            void  synapse_weights(vector <uint32_t> &pres,
                     vector <uint32_t> &posts,
                     vector <double> &vals,
-                    int network_id = 0);
+                    int network_id)
+            {
+                return get_risp_network(network_id)->synapse_weights(pres, posts, vals);
+            }
 
-            void clear_activity(int network_id = 0);
+            void clear_activity(int network_id) {
+                get_risp_network(network_id)->clear_activity();
+            }
 
-            string get_name() const;
+            string get_name() const {
+                return "risp";
+            }
 
+            Network* get_risp_network(int network_id) 
+            {
+                map <int, risp::Network*>::const_iterator it;
+                it = networks.find(network_id);
+                return it->second;
+            }
         protected:
 
-            risp::Network* get_risp_network(int network_id);
             double get_input_spike_factor() const;
             map <int, risp::Network*> networks;
 
